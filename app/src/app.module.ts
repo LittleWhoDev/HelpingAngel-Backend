3ede738f2fe919eea9d6d963c730160bcab82c36
JustBeYou <mihailferaru2000@gmail.com>
2020-09-04 19:39:04 +0300

    ops: configure database

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbConfig = {
+          host: configService.get<string>('DB_HOST'),
+          name: configService.get<string>('DB_NAME'),
+          user: configService.get<string>('DB_USER'),
+          pass: configService.get<string>('DB_PASSWORD'),
+          port: configService.get<number>('DB_PORT'),
+        };
+
+        return {
+          uri: `mongodb://${dbConfig.user}:${dbConfig.pass}@${dbConfig.host}:${dbConfig.port}/${dbConfig.name}?authSource=admin`,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
